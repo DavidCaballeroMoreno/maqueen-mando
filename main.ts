55daadaa94bcb74641c78476791fa8c9c4dc8c86
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-04-04 11:12:55 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(999)
+basic.forever(function () {
+	
+})
