f7b176c1ab9df7627d477973167aa7029f3b8b42
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-04-20 09:26:29 +0200

    Terminado

--- main.ts
+++ main.ts
@@ -9,6 +9,9 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(3)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
@@ -16,3 +19,6 @@ input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
 radio.setGroup(999)
+basic.showString("Hola")
+music.playMelody("C D E F G A B C5 ", 120)
+music.playMelody("C5 B A G F E D C ", 120)
